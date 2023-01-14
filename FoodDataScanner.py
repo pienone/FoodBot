5927fc8e0f0ace6a2acfa93ad825afbc8c82a4fe
Pietro Nonis <61913424+pienone@users.noreply.github.com>
2023-01-14 11:32:18 +0100

    Agribalyse values printout function

--- FoodDataScanner.py
+++ FoodDataScanner.py
@@ -1,5 +1,3 @@
-
-#%%
 import telegram
 import telegram.ext
 from io import BytesIO
@@ -9,14 +7,14 @@ from telegram.ext import MessageHandler, Filters
 from io import BytesIO
 import numpy as np
 import requests
-from .config import settings
+from config import settings
 
 
 # Dispatcher
 updater = telegram.ext.Updater(settings.API_KEY_TELEGRAM_FOOD)
 dispatcher = updater.dispatcher
 
-# decode barcode image
+# Decode barcode image
 def decode(image):
     img =cv2.imdecode(np.frombuffer(image.read(), np.uint8), 1)
     try:
@@ -27,43 +25,49 @@ def decode(image):
     except:
         return(None)
 
-# use openfoodfacts API
-def get_categories_names(barcode):
+# Print message for telegram chat
+def make_perc_table(dictlist):
+    co2_cat=list(np.array(list(dictlist.keys()))[np.array(['co2' in x for x in dictlist.keys()])])
+    co2_cat.remove('co2_total')
+    mess=f"Total Co2 emissions: {dictlist['co2_total']*100:.2f} gCO2/100g \n"
+    for cat in co2_cat:
+            mess=mess+f"{cat.split('_')[1][0].upper() + cat.split('_')[1][1:]}: {dictlist[cat]/dictlist['co2_total']*100:.2f}% \n"
+    return(mess)
+
+# Retrieve Agribalyse data from openfoodfacts API
+def get_CO2(barcode):
     requestURL="https://world.openfoodfacts.org/api/v2/product/"+barcode+"&json=true"
     response=requests.get(requestURL)
     data = response.json()
     try:
-        ingr=data['product']['ingredients_text']
-        allerg=data['product']['allergens']
-        nutriscore=data['product']['nutriscore_grade']
-        print(ingr,allerg,nutriscore)
-        return(ingr,allerg,nutriscore)
+        #ingr=data['product']['ingredients_text']
+        #allerg=data['product']['allergens']
+        #nutriscore=data['product']['nutriscore_grade']
+        agrib=data['product']['ecoscore_data']['agribalyse']
+        return(agrib)
     except:
-        return('Food not found')
+        return('Food does not have emission data')
 
-# use Openfoodfacts API
+# Main photo to message
 def photo(update, context):
     file = context.bot.get_file(update.message.photo[-1].file_id)
-    print('ok')
     f =  BytesIO(file.download_as_bytearray())
     result = decode(f)
     if result==None:
         response = 'No barcode found, please send a better picture'
     else:
         try:
-            ingr,allerg,nutriscore=get_categories_names(result)
-            response = f'The barcode is {result}, nutriscore: {nutriscore}. \nIngredients are {ingr}. \nAllergens: {allerg}.'
+            co2data=get_CO2(result)
+            response = make_perc_table(co2data)
+            #response = f'The barcode is {result}, nutriscore: {nutriscore}. \nIngredients are {ingr}. \nAllergens: {allerg}.'
         except:
-            response = 'Food not found'
+            response = 'Food does not have emission data'
     context.bot.send_message(chat_id=update.message.chat_id, text=response)
 
-# photo handler
+# hoto message handler
 photo_handler = MessageHandler(Filters.photo, photo)
 dispatcher.add_handler(photo_handler)
 
-# start polling for updates from Telegram
 updater.start_polling()
 
-# block until a signal (like one sent by CTRL+C) is sent
 updater.idle()
-
